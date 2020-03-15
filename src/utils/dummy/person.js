3af3bd4cd3acb6fd76247d98da4d7401dd898467
Matthew Wong <mattchw@Matthews-MacBook-Pro.local>
2020-03-15 16:55:16 +0800

    add contentItem detail: stance bar, occupation chips

---
+++ src/utils/dummy/person.js
@@ -0,0 +1,36 @@
+export default [{
+    name: "abc",
+    stance: 1,
+    occupation: [
+      1, 2, 3, 4
+    ],
+    description: "adsfdfadwfwfwvwvwvevwevvwrvrwwwevwfdafad"
+  },{
+    name: "abc",
+    stance: 2, 
+    occupation: [
+      1, 4
+    ],
+    description: "adsfdfadfdafad"
+  },{
+    name: "abc",
+    stance: 3, 
+    occupation: [
+      3, 4
+    ],
+    description: "adsfdfadfdafad"
+  },{
+    name: "abc", 
+    stance: 1,
+    occupation: [
+      1
+    ],
+    description: "adsfdfadfdafad"
+  },{
+    name: "abc", 
+    stance: 2,
+    occupation: [
+      1
+    ],
+    description: "adsfdfadfdafad"
+  }];
---
+++ src/utils/stance.js
@@ -0,0 +1,13 @@
+export default [{
+    id: 1,
+    name: 'Yellow',
+    value: '#ffdf00',
+},{
+    id: 2,
+    name: 'Blue',
+    value: '#0057c3',
+},{
+    id: 3,
+    name: 'Unknown',
+    value: '#929292',
+}];
